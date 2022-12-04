97cba9a4f7b61fb358f560e52e4c0f3f02e58ef5
nothing78 <tomcatkomunitas@gmail.com>
2022-12-04 12:33:29 +0700

    untuk projek akhir

--- TugasAkhirGrafkom/BreakBlock.py
+++ TugasAkhirGrafkom/BreakBlock.py
@@ -2,129 +2,125 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
-FromRight = 1
-FromLeft = 2
-FromTop = 3
-FromBottom = 4
-windowWidth = 800
+fromRight = 1
+fromLeft = 2
+fromTop = 3
+fromBottom = 4
+windowWitdh = 800
 windowHeight = 500
-DeltaX = 1
-DeltaY = 1
-TimeInterval = 5
+deltaX = 1
+deltaY = 1
+time_interval = 5
 
-class Block:
-    def __init__(self, right, left, top, bottom):
-        self.right = right
+class block:
+    def __init__(self, left, bottom, right, top):
         self.left = left
-        self.top = top
         self.bottom = bottom
+        self.right = right
+        self.top = top
 
-ball = Block(100, 100, 120, 120)
-border = Block(0, 0, windowWidth, windowHeight)
-stick = Block(370, 0, 470, 10)
-side1 = Block(0, 0, 10, 280)
-side2 = Block(790, 0, 800, 280)
-sideX = 0
-sideY = 350
-BlockList = []
-SideList = []
+ball = block(100, 100, 120, 120)
+border = block(0, 0, windowWitdh, windowHeight)
+Stick = block(370, 0, 470, 10) 
+side1 = block(0, 0, 10, 280)
+side2 = block(790, 0, 800, 280)
+blockList = []
+sidelist=[]
 x = 15
 y = 350
-
-def init():
-    glClearColor(1, 0, 0, 0)
-
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glOrtho(0, windowWidth, 0, windowHeight, 0, 1)
-
-    glMatrixMode(GL_MODELVIEW)
+sideX=0
+sideY=350
 
 for i in range(0, 156):
     if (i % 13 == 0):
         x = 15
         y += 30
-    if (i % 39 == 0):
-        y += 50
-    
-    BlockList.append(Block(x, y, x + 50, y + 20))
+    if(i%39==0):
+        y+=50
+
+    blockList.append(block(x, y, x + 50, y + 20))
     x += 60
 
-    Move = 0
-    Pause = True
-
-def DefaultValues():
-    global SideList
-    global BlockList
+def defaultValues():
+    global sidelist
+    global blockList
     global x
     global y
+    global move
+    global pause
     global sideX
     global sideY
-    global Move
-    global Pause
+    blockList.clear()
+    sidelist.clear()
 
-    for z in range (0, 156):
-        if (z % 13 == 0):
-            x = 15 
+    move=0
+    pause = True
+    x = 15
+    y = 350
+
+    for i in range(0, 156):
+        if (i % 13 == 0):
+            x = 15
             y += 30
-        if (z % 39 == 0):
+        if (i % 39 == 0):
             y += 50
-        BlockList.append(Block(x, y, x + 50, y + 20))
+        blockList.append(block(x, y, x + 50, y + 20))
         x += 60
 
-def DrawRectangle(rectangle, r, g, b):
+def init():
+    glClearColor(255, 255, 255, 1)
+
+    glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    glColor(r, g, b)
-    glColor(0, 255, 255)
+    glOrtho(0, windowWitdh, 0, windowHeight, 0, 1)  
+
+    glMatrixMode(GL_MODELVIEW)
+
+def Drawrectangleangle(rectangle,r,g,b):
+    glLoadIdentity()
+    glColor(0,0,255)
 
     glBegin(GL_QUADS)
-    glVertex(rectangle.left, rectangle.bottom, 0)
+    glVertex(rectangle.left, rectangle.bottom, 0)  # Left - Bottom
     glVertex(rectangle.right, rectangle.bottom, 0)
     glVertex(rectangle.right, rectangle.top, 0)
     glVertex(rectangle.left, rectangle.top, 0)
     glEnd()
 
-def DrawText(String, x, y):
+def drawText(string, x, y):
     glLineWidth(2)
-    glColor(255, 255, 0)
-    glLoadIdentity()
+    glColor(0, 255, 0)  
+    glLoadIdentity()  
     glTranslate(x, y, 0)
     glScale(0.13, 0.13, 1)
-    String = String.encode()
-    for c in String:
+    string = string.encode()  
+    for c in string:
         glutStrokeCharacter(GLUT_STROKE_ROMAN, c)
 
-def DrawEndText(String, x, y):
+def drawEndText(string, x, y):
     glLineWidth(2.5)
-    glColor(255, 255, 0)
-    glLoadIdentity()
+    glColor(255,0,0)  
+    glLoadIdentity()  
     glTranslate(x, y, 0)
     glScale(0.15, 0.15, 1)
-    String = String.encode()
-    for c in String:
+    string = string.encode()  
+    for c in string:
         glutStrokeCharacter(GLUT_STROKE_ROMAN, c)
 
-def BallAndPlayer(Ball, PLayer):
-    if Ball.bottom <= PLayer.top and Ball.left >= PLayer.left and Ball.right <= PLayer.right:
-        return True
-    return False
+def BallAndWall(ball, wall):  
+    global fromRight
+    global fromLeft
+    global fromTop
+    global fromBottom
 
-exit = False
-
-def BallAndWall(Ball, Wall):
-    global FromRight
-    global FromLeft
-    global FromTop
-    global FromBottom
-
-    if Ball.right >= Wall.right - 14:
-        return FromRight
-    if Ball.left <= Wall.left + 14:
-        return FromLeft
-    if Ball.top >= Wall.top - 14:
-        return FromTop
-    if Ball.bottom <= Wall.bottom:
-        return FromBottom
+    if ball.right >= wall.right-14:
+        return fromRight
+    if ball.left <= wall.left+14:
+        return fromLeft
+    if ball.top >= wall.top-14:
+        return fromTop
+    if ball.bottom <= wall.bottom:
+        return fromBottom
 
 def BallAndPlayer(ball, player):  
     if ball.bottom <= player.top and ball.left >= player.left and ball.right <= player.right:
