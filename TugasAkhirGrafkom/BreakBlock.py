f982331be30abd9f34db76cb097787d57972bd34
nothing78 <tomcatkomunitas@gmail.com>
2022-12-04 10:08:21 +0700

    untuk projek akhir

--- TugasAkhirGrafkom/BreakBlock.py
+++ TugasAkhirGrafkom/BreakBlock.py
@@ -126,3 +126,144 @@ def BallAndWall(Ball, Wall):
     if Ball.bottom <= Wall.bottom:
         return FromBottom
 
+def BallAndPlayer(ball, player):  
+    if ball.bottom <= player.top and ball.left >= player.left and ball.right <= player.right:
+        return True
+    return False
+
+exit = False
+
+def keyboard(key, x, y):
+    global pause
+    global Lives
+    global exit
+    global score
+    if key == b"q":
+        exit = True
+    elif key == b" ":
+        pause = not pause
+    elif key == b"p":
+        glClearColor(0, 0, 0, 1)
+        exit = False
+        score=0
+        Lives = 3
+        defaultValues()
+
+mouse_x = 0
+def MouseMotion(x, y):
+    global mouse_x
+    mouse_x = x
+
+def Timer(v):
+    Display()
+
+    glutTimerFunc(time_interval, Timer, 1)
+
+Lives = 3
+pause = True
+score= 0
+move=0
+
+def Display():
+    global score
+    global Lives
+    global playerResult
+    global fromRight
+    global fromLeft
+    global fromTop
+    global fromBottom
+    global deltaX
+    global deltaY
+    global pause
+    global blockList
+    global move
+    global sidelist
+
+    if (Lives > 0 and exit == False):
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        if (pause and Lives>0):
+            string = "press space to play ' Q ' to exit"
+            drawText(string, 250, 250)
+
+        Drawrectangleangle(block(0,windowHeight-14,windowWitdh,windowHeight),0,0,1)
+        Drawrectangleangle(block(0,windowHeight-120,14,windowHeight-10),0,0,1)
+        Drawrectangleangle(block(windowWitdh-14,windowHeight-120,windowWitdh,windowHeight-10),0,0,1)
+        Drawrectangleangle(block(0, windowHeight - 240, 14, windowHeight - 120), 1, 1, 1)
+        Drawrectangleangle(block(windowWitdh-14,windowHeight-240,windowWitdh,windowHeight-120),1,1,1)
+        Drawrectangleangle(block(0, windowHeight - 360, 14, windowHeight - 240), 1, 1, 0)
+        Drawrectangleangle(block(windowWitdh - 14, windowHeight - 360, windowWitdh, windowHeight - 240), 1, 1, 0)
+        Drawrectangleangle(block(0, windowHeight - 490, 14, windowHeight - 360), 1, 0, 0)
+        Drawrectangleangle(block(windowWitdh - 14, windowHeight - 490, windowWitdh, windowHeight - 360), 1, 0, 0)
+        Drawrectangleangle(block(0, 0, 14, 10), 0, 1, 0)
+        Drawrectangleangle(block(windowWitdh - 14, 0 , windowWitdh, 10), 0, 1, 0)
+       
+        for x in blockList:  
+            r = 0
+            b = 1
+            g = 0
+            if (x.bottom <= windowHeight - 120 and x.bottom > windowHeight - 240):
+                r = 1
+                b = 1
+                g = 1
+            elif (x.bottom <= windowHeight - 240 and x.bottom > windowHeight - 360):
+                r = 1
+                b = 0
+                g = 1
+            elif (x.bottom <= windowHeight - 360 and x.bottom > windowHeight - 490):
+                r = 1
+                b = 0
+                g = 0
+
+            Drawrectangleangle(x, r, g, b)
+
+        if (pause == False):
+
+            move += 10
+            if (move % 300 == 0):
+                for x in blockList:
+                    x.bottom -= 1
+                    x.top -= 1
+                for x in sidelist:
+                    x.bottom-=1
+                    x.top-=1
+
+            ball.left = ball.left + deltaX  
+            ball.right = ball.right + deltaX
+            ball.top = ball.top + deltaY
+            ball.bottom = ball.bottom + deltaY
+
+            if BallAndPlayer(ball, Stick) == True:
+                if (Stick.right - 50 < ball.left):
+                    deltaY = 1
+                    deltaX = 1
+                else:
+                    deltaY = 1
+                    deltaX = -1
+
+            # Ini untuk bola dan dinding
+            if BallAndWall(ball, border) == fromRight:
+                deltaX = -1
+
+            if BallAndWall(ball, border) == fromLeft:
+                deltaX = 1
+
+            if BallAndWall(ball, border) == fromTop:
+                deltaY = -1
+
+            if BallAndWall(ball, border) == fromBottom:
+                deltaY = 1
+                Lives = Lives - 1
+
+            for x in blockList:
+                if (x.left == 0):
+                    score += 1
+                    x.left = -10
+                elif(x.right==-5):
+                    score-=1
+                    x.right=-4
+                if(x.bottom==Stick.top):
+                    x.bottom=-5
+                    x.top=-5
+                    x.left=-5
+                    x.right=-5
